bc6b1e791900ca2fea7495258e6eac78072091d7
kakaoleaves <kakaoleaves@naver.com>
2023-04-21 23:34:12 +0900

    firebase 구글연동 테스트 완료

--- frontend/TravelAI/src/screens/OnBoardFirstScreen.js
+++ frontend/TravelAI/src/screens/OnBoardFirstScreen.js
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {View, Text, StyleSheet} from 'react-native';
 import Button from '../component/atoms/Button';
 import {useNavigation} from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId: '24378092542-l46d9sch9rgn6d2th6hj880q3841o3ml.apps.googleusercontent.com',
+});
+
+async function onGoogleButtonPress() {
+  // Check if your device supports Google Play
+  await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+  // Get the users ID token
+  const {idToken} = await GoogleSignin.signIn();
+
+  // Create a Google credential with the token
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+  // Sign-in the user with the credential
+  return auth().signInWithCredential(googleCredential);
+}
 
 function OnBoardFirstScreen(props) {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
   const navigation = useNavigation();
   const onPressButton = () => {
     navigation.navigate('OnBoardSecond');
   };
+
+  if (initializing) return null;
 
   return (
     <SafeAreaView style={styles.safearea}>
       <View style={styles.container}>
         <Text>OnBoardFirstScreen</Text>
         <Button onPress={onPressButton} title={'Next'} />
+        {!user ? (
+          <Button
+            title="Google Sign-In"
+            onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+          />
+        ) : (
+          <Text>{user.email}</Text>
+        )}
       </View>
     </SafeAreaView>
   );
