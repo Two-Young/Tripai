5562990f9682ec8a0e7c51bae7e8cd65167f8f43
kakaoleaves <kakaoleaves@naver.com>
2023-10-09 00:17:08 +0900

    [FRONTEND] DRAWER 아이콘 변경

--- frontend/TravelAI/src/component/organisms/MenuDrawer.js
+++ frontend/TravelAI/src/component/organisms/MenuDrawer.js
@@ -73,11 +73,15 @@ const MenuDrawer = props => {
           />
           <Text style={styles.userNameText}>{userName}</Text>
         </View>
-        <Drawer.Item label="Profile" onPress={navigateToProfile} icon="star" />
-        <Drawer.Item label="Default Currency" onPress={navigateToDefaultCurrency} icon="star" />
-        <Drawer.Item label="Friends" onPress={navigateToPeople} icon="star" />
-        <Drawer.Item label="Session Requests" onPress={navigateToMySessionRequest} icon="star" />
-        <Drawer.Item label="Logout" onPress={logout} icon="star" />
+        <Drawer.Item label="Profile" onPress={navigateToProfile} icon="account" />
+        <Drawer.Item
+          label="Default Currency"
+          onPress={navigateToDefaultCurrency}
+          icon="account-cash"
+        />
+        <Drawer.Item label="Friends" onPress={navigateToPeople} icon="account-multiple" />
+        <Drawer.Item label="Session Requests" onPress={navigateToMySessionRequest} icon="mail" />
+        <Drawer.Item label="Logout" onPress={logout} icon="logout" />
       </Drawer.Section>
     </Modal>
   );
