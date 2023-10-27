5565bf0ba5e1ab573e96bbac601a4e1c969961e0
kakaoleaves <kakaoleaves@naver.com>
2023-10-27 15:16:30 +0900

    iOS 키보드 테스트 완료

--- frontend/TravelAI/src/screens/AddTravelScreen.js
+++ frontend/TravelAI/src/screens/AddTravelScreen.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View, FlatList, Image, TouchableWithoutFeedback} from 'react-native';
+import {StyleSheet, Text, View, FlatList, Image} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {useRecoilState} from 'recoil';
